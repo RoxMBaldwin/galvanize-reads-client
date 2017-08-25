a3c2e149ef63260f6c4822204af8c3c1e87bdbf2
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2017-08-24 21:17:39 -0600

    📚  User can add new books

--- books.js
+++ books.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 
 })
 
-baseURL = 'https://rocky-harbor-92850.herokuapp.com/books/'
+// const baseURL = 'https://rocky-harbor-92850.herokuapp.com/books/'
+const baseURL = 'http://localhost:5000/books/'
 
 function newBook(event){
   let newBookForm = `
@@ -12,26 +13,26 @@ function newBook(event){
     <form class="col s12">
       <div class="row">
         <div class="input-field col s6">
-          <input id="id" type="text" class="validate">
-          <label for="first_name">Title</label>
+          <input id="id" type="text" class="title">
+          <label for="title">Title</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <input id="id" type="text" class="validate">
-          <label for="first_name">Genre</label>
+          <input id="id" type="text" class="genre">
+          <label for="genre">Genre</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input type="text" class="validate">
-          <label for="disabled">Description</label>
+          <input type="text" class="description">
+          <label for="description">Description</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">book cover url</label>
+          <input id="password" type="text" class="coverURL">
+          <label for="coverURL">book cover url</label>
         </div>
       </div>
       </div>
@@ -41,19 +42,35 @@ function newBook(event){
 
   let allBooksButton = `
   <div class="allBook">
-    <button class="waves-effect waves-light btn-large allBooksButton" type="button" name="button"><i class="material-icons left">book</i></button>
-    <h4>see all books</h4>
+    <button class="waves-effect waves-light btn-large saveBookButton" type="button" name="button"><i class="material-icons left">book</i></button>
+    <h4>save book</h4>
   </div>`
 
-  $('.newBookForm').html(newBookForm)
+  $('.appendAllBooks').html(newBookForm)
 
   $('.newBook').html(allBooksButton)
 
-  $('.allBooksButton').click((event) => {
+  $('.saveBookButton').click(function(){
+    var postBody = {
+      title: $('.title').val(),
+      genre: $('.genre').val(),
+      description: $('.description').val(),
+      coverURL: $('.coverURL').val()
+    }
+    $.post(baseURL, postBody).then(data => {
+      window.location.reload()
+    })
+  })
+
+  // $('.saveBookButton').click((event) => {
+  //   window.relocation.reload()
+  // })
+
+  // $('.allBooksButton').click((event) => {
     // $('.newBookForm').empty()
     // $.get(baseURL, allBooks)
-    window.relocation.reload()
-  })
+    //window.relocation.reload()
+  // })
   ///delete all this and do a window reload duhhhhh
 }
 
